3cc1042e28401c245e1554634d607745b8da1f45
Jezreel Jann V. Yañez <jezreelvyanez@gmail.com>
2025-04-17 16:10:23 +0800

    feat: raise error when email is not provided on registration

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,3 +27,8 @@ class ModelTest(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "testpass@123")
