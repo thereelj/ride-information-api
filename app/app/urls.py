8b026f253ef9e7ff1e33a63416e8d8f6a6aac089
Jezreel Jann V. Yañez <jezreelvyanez@gmail.com>
2025-04-17 18:30:15 +0800

    chore: setup project documentation

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/schecma/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
 ]
