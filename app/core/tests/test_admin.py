cc902567a4c8657a305694c975ca56ab4473a8c0
Jezreel Jann V. Yañez <jezreelvyanez@gmail.com>
2025-04-17 17:58:44 +0800

    fix: view error upon viewing single user in django-admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,42 @@
+"""Test for the django admin modifications"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Tests for django admin"""
+
+    def setUp(self):
+        """Create user and client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@email.com", password="Test@123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@email.com",
+            password="Test@123",
+            first_name="John",
+            last_name="Doe",
+            phone_number="1-800-000-0000",
+        )
+
+    def test_users_list(self):
+        """Test that users are listed on page."""
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.first_name)
+        self.assertContains(res, self.user.last_name)
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.phone_number)
+
+    def test_edit_user_page(self):
+        """Test the eidt user page works."""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
