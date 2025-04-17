769e0e8c595b628a7935a48982254f78e48f9e00
Jezreel Jann V. Yañez <jezreelvyanez@gmail.com>
2025-04-17 16:04:52 +0800

    chore: fix format with black

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,11 +19,11 @@ class ModelTest(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users"""
         sample_emails = [
-            ['test1@EMAIL.com', 'test1@email.com'],
-            ['Test2@Email.com', 'Test2@email.com'],
-            ['TEST3@EMAIL.COM', 'TEST3@email.com'],
-            ['test4@email.COM', 'test4@email.com']
+            ["test1@EMAIL.com", "test1@email.com"],
+            ["Test2@Email.com", "Test2@email.com"],
+            ["TEST3@EMAIL.COM", "TEST3@email.com"],
+            ["test4@email.COM", "test4@email.com"],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
