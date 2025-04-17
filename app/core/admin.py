942da31feefefd82280ba629362a28e76363f82a
Jezreel Jann V. Yañez <jezreelvyanez@gmail.com>
2025-04-17 18:07:05 +0800

    chore: format fieldsets for adding user in django-admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,13 +11,32 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
     ordering = ["id"]
-    list_display = ["email", "first_name", "last_name", "phone_number"]
+    list_display = ["email", "first_name", "last_name", "phone_number", "role"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
         (_("Important dates"), {"fields": ("last_login",)}),
     )
     readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "phone_number",
+                    "role",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
