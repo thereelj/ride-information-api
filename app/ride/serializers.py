4b47f410be05629a48f6eae936a028090831a9f2
Jezreel Jann V. Yañez <jezreelvyanez@gmail.com>
2025-04-18 11:36:27 +0800

    feat: add distance in ride response

--- app/ride/serializers.py
+++ app/ride/serializers.py
@@ -8,9 +8,27 @@ from core.models import Ride, RideEvent
 class RideSerializer(serializers.ModelSerializer):
     """Ride object serializer"""
 
+    distance = serializers.SerializerMethodField()
+
     class Meta:
         model = Ride
-        fields = "__all__"
+        fields = [
+            "id",
+            "status",
+            "pickup_latitude",
+            "pickup_longitude",
+            "dropoff_latitude",
+            "dropoff_longitude",
+            "pickup_time",
+            "id_rider",
+            "id_driver",
+            "distance",
+        ]
+
+    def get_distance(self, obj):
+        if hasattr(obj, "distance") and obj.distance is not None:
+            return f"{round(obj.distance, 2)} km"
+        return None
 
 
 class RideEventSerializer(serializers.ModelSerializer):
